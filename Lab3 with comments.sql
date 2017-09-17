8295e2b5063ede692c385890ff667280ac92fbc6
ndepaul1 <nicholas.depaul1@marist.edu>
2017-09-17 15:48:34 -0400

    Lab 3 revised

---
+++ Lab3 with comments.sql
@@ -0,0 +1,56 @@
+--Nick DePaul
+--9/18/2017
+--Lab 3
+
+select ordno, totalusd
+from Orders;
+
+select name, city
+from Agents
+where name='Smith';
+
+select pid, name, priceusd
+from Products
+where qty>'200010';
+
+select name, city
+from Customers
+where city='Duluth';
+
+select name
+from Agents
+where not city='Dallas'
+	intersect
+select name
+from Agents
+where not city='Duluth';
+
+select *
+from Products
+where not city ='Dallas'
+	intersect
+select *
+from Products 
+where not city = 'Duluth'
+	intersect
+select *
+from Products
+where priceusd<'1.00';
+
+select *
+from Orders
+where month='Mar' or month='May';
+
+select *
+from Orders
+where month='Feb'
+	intersect
+select *
+from Orders
+where totalusd>='500.00';
+
+select Orders.*
+from Orders
+inner join Customers
+on Orders.cid = Customers.cid
+where Orders.cid='c005';
